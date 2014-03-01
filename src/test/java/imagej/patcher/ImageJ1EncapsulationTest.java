acee2b6057465da772ffdb79972dbac701800e0b
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-01 00:35:43 -0600

    Add a unit test ensuring IJ1 is used judiciously
    We only want EssentialLegacyHooks and HeadlessGenericDialog to actually
    use the ImageJ 1.x classes. All other classes (outside unit tests, of
    course) should not access any ImageJ 1.x class in any way because that
    would wreak havoc in situations where different ClassLoader instances are
    used to load the patcher and ImageJ 1.x.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

---
+++ src/test/java/imagej/patcher/ImageJ1EncapsulationTest.java
@@ -0,0 +1,205 @@
+/*
+ * #%L
+ * ImageJ software for multidimensional image processing and analysis.
+ * %%
+ * Copyright (C) 2009 - 2014 Board of Regents of the University of
+ * Wisconsin-Madison, Broad Institute of MIT and Harvard, and Max Planck
+ * Institute of Molecular Cell Biology and Genetics.
+ * %%
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ * #L%
+ */
+
+package imagej.patcher;
+
+import static org.junit.Assert.assertEquals;
+
+import java.net.URL;
+import java.util.HashSet;
+import java.util.Set;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.CtConstructor;
+import javassist.CtField;
+import javassist.CtMethod;
+import javassist.NotFoundException;
+import javassist.expr.Cast;
+import javassist.expr.ConstructorCall;
+import javassist.expr.ExprEditor;
+import javassist.expr.FieldAccess;
+import javassist.expr.Handler;
+import javassist.expr.Instanceof;
+import javassist.expr.MethodCall;
+import javassist.expr.NewArray;
+import javassist.expr.NewExpr;
+
+import org.junit.Test;
+
+/**
+ * Verifies that ImageJ 1.x classes are not used everywhere.
+ * 
+ * @author Johannes Schindelin
+ */
+public class ImageJ1EncapsulationTest {
+
+	@Test
+	public void verifyEncapsulation() throws Exception {
+		final ClassPool pool = ClassPool.getDefault();
+
+		final Set<String> exceptions = new HashSet<String>();
+		exceptions.add(EssentialLegacyHooks.class.getName());
+		exceptions.add(HeadlessGenericDialog.class.getName());
+
+		final URL directory = Utils.getLocation(Utils.class);
+		final int prefixLength = directory.toString().length();
+		for (final URL url : Utils.listContents(directory)) {
+			final String path = url.toString().substring(prefixLength);
+			if (!path.endsWith(".class")) continue;
+			final String className = path.substring(0, path.length() - 6).replace('/', '.');
+
+			if (exceptions.contains(className)) {
+				exceptions.remove(className);
+			}
+			else try {
+				final CtClass clazz = pool.get(className);
+				clazz.instrument(new ImageJ1UsageTester());
+			} catch (final Exception e) {
+				throw new RuntimeException("Problem with class " + className, e);
+			}
+		}
+
+		assertEquals(exceptions.toString(), 0, exceptions.size());
+	}
+
+	private final class ImageJ1UsageTester extends ExprEditor {
+
+		private void test(final CtClass c) {
+			if (c != null && c.getName().startsWith("ij.")) {
+				throw new RuntimeException("ImageJ 1.x class used: " + c.getName());
+			}
+		}
+
+		@Override
+		public void edit(Cast c) {
+			try {
+				test(c.getType());
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(ConstructorCall c) {
+			try {
+				test(c.getConstructor().getDeclaringClass());
+				final CtConstructor c2 = c.getConstructor();
+				for (final CtClass c3 : c2.getExceptionTypes()) {
+					test(c3);
+				}
+				for (final CtClass c3 : c2.getParameterTypes()) {
+					test(c3);
+				}
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(FieldAccess f) {
+			try {
+				final CtField field = f.getField();
+				test(field.getDeclaringClass());
+				test(field.getType());
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(Handler h) {
+			try {
+				test(h.getType());
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(Instanceof i) {
+			try {
+				test(i.getType());
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(MethodCall m) {
+			try {
+				final CtMethod m2 = m.getMethod();
+				test(m2.getDeclaringClass());
+				test(m2.getReturnType());
+				for (final CtClass c2 : m2.getExceptionTypes()) {
+					test(c2);
+				}
+				for (final CtClass c2 : m2.getParameterTypes()) {
+					test(c2);
+				}
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(NewArray a) {
+			try {
+				test(a.getComponentType());
+			}
+			catch (NotFoundException e) {
+				throw new RuntimeException(e);
+			}
+		}
+
+		@Override
+		public void edit(NewExpr e) {
+			try {
+				final CtConstructor c = e.getConstructor();
+				for (final CtClass c2 : c.getExceptionTypes()) {
+					test(c2);
+				}
+				for (final CtClass c2 : c.getParameterTypes()) {
+					test(c2);
+				}
+			}
+			catch (NotFoundException e2) {
+				throw new RuntimeException(e2);
+			}
+		}
+	}
+}
