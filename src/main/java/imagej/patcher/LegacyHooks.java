8a7d1473d9272c959fcd023fac0df2e786318545
Johannes Schindelin <johannes.schindelin@gmx.de>
2014-03-02 20:56:39 -0600

    Clean up source in LegacyHooks
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- src/main/java/imagej/patcher/LegacyHooks.java
+++ src/main/java/imagej/patcher/LegacyHooks.java
@@ -56,8 +56,7 @@ public abstract class LegacyHooks {
 	/**
 	 * Determines whether the image windows should be displayed or not.
 	 * 
-	 * @return false if ImageJ 1.x should be prevented from opening image
-	 *         windows.
+	 * @return false if ImageJ 1.x should be prevented from opening image windows.
 	 */
 	public boolean isLegacyMode() {
 		return true;
@@ -66,8 +65,8 @@ public abstract class LegacyHooks {
 	/**
 	 * Return the current context, if any.
 	 * <p>
-	 * For ImageJ2-specific hooks, the returned object will be the current
-	 * SciJava context, or null if the context is not yet initialized.
+	 * For ImageJ2-specific hooks, the returned object will be the current SciJava
+	 * context, or null if the context is not yet initialized.
 	 * </p>
 	 * 
 	 * @return the context, or null
@@ -106,91 +105,74 @@ public abstract class LegacyHooks {
 	/**
 	 * Intercepts the call to {@link ij.IJ#runPlugIn(String, String)}.
 	 * 
-	 * @param className
-	 *            the class name
-	 * @param arg
-	 *            the argument passed to the {@code runPlugIn} method
+	 * @param className the class name
+	 * @param arg the argument passed to the {@code runPlugIn} method
 	 * @return the object to return, or null to let ImageJ 1.x handle the call
 	 */
-	public Object interceptRunPlugIn(String className, String arg) {
+	public Object interceptRunPlugIn(final String className, final String arg) {
 		return null;
 	}
 
 	/**
 	 * Updates the progress bar, where 0 <= progress <= 1.0.
 	 * 
-	 * @param value
-	 *            between 0.0 and 1.0
+	 * @param value between 0.0 and 1.0
 	 */
-	public void showProgress(double progress) {
-	}
+	public void showProgress(final double progress) {}
 
 	/**
 	 * Updates the progress bar, where the length of the bar is set to (
-	 * <code>currentValue + 1) / finalValue</code> of the maximum bar length.
-	 * The bar is erased if <code>currentValue &gt;= finalValue</code>.
+	 * <code>currentValue + 1) / finalValue</code> of the maximum bar length. The
+	 * bar is erased if <code>currentValue &gt;= finalValue</code>.
 	 * 
-	 * @param currentIndex
-	 *            the step that was just started
-	 * @param finalIndex
-	 *            the final step.
+	 * @param currentIndex the step that was just started
+	 * @param finalIndex the final step.
 	 */
-	public void showProgress(int currentIndex, int finalIndex) {
-	}
+	public void showProgress(final int currentIndex, final int finalIndex) {}
 
 	/**
 	 * Shows a status message.
 	 * 
-	 * @param status
-	 *            the message
+	 * @param status the message
 	 */
-	public void showStatus(String status) {
-	}
+	public void showStatus(final String status) {}
 
 	/**
 	 * Logs a message.
 	 * 
-	 * @param message
-	 *            the message
+	 * @param message the message
 	 */
-	public void log(String message) {
-	}
+	public void log(final String message) {}
 
 	/**
 	 * Registers an image (possibly not seen before).
 	 * 
-	 * @param image
-	 *            the new image
+	 * @param image the new image
 	 */
-	public void registerImage(final Object image) {
-	}
+	public void registerImage(final Object image) {}
 
 	/**
 	 * Releases an image.
 	 * 
-	 * @param imagej
-	 *            the image
+	 * @param imagej the image
 	 */
-	public void unregisterImage(final Object image) {
-	}
+	public void unregisterImage(final Object image) {}
 
 	/**
 	 * Logs a debug message (to be shown only in debug mode).
 	 * 
-	 * @param string
-	 *            the debug message
+	 * @param string the debug message
 	 */
-	public void debug(String string) {
+	public void debug(final String string) {
 		System.err.println(string);
 	}
 
 	/**
 	 * Shows an exception.
 	 * 
-	 * @param t
-	 *            the exception
+	 * @param t the exception
 	 */
-	public void error(Throwable t) {
+	public void error(final Throwable t) {
 		// ignore
 	}
 
@@ -215,24 +197,21 @@ public abstract class LegacyHooks {
 	/**
 	 * Extension point to override ImageJ 1.x' editor.
 	 * 
-	 * @param path
-	 *            the path to the file to open
+	 * @param path the path to the file to open
 	 * @return true if the hook opened a different editor
 	 */
-	public boolean openInEditor(String path) {
+	public boolean openInEditor(final String path) {
 		return false;
 	}
 
 	/**
 	 * Extension point to override ImageJ 1.x' editor.
 	 * 
-	 * @param fileName
-	 *            the name of the new file
-	 * @param content
-	 *            the initial content
+	 * @param fileName the name of the new file
+	 * @param content the initial content
 	 * @return true if the hook opened a different editor
 	 */
-	public boolean createInEditor(String fileName, String content) {
+	public boolean createInEditor(final String fileName, final String content) {
 		return false;
 	}
 
@@ -251,11 +230,14 @@ public abstract class LegacyHooks {
 			return result;
 		}
 		final String userHome = System.getProperty("user.home");
-		if (userHome != null) handleExtraPluginJars(new File(userHome, ".plugins"), result);
+		if (userHome != null) handleExtraPluginJars(new File(userHome, ".plugins"),
+			result);
 		return result;
 	}
 
-	private void handleExtraPluginJars(final File directory, final List<File> result) {
+	private void handleExtraPluginJars(final File directory,
+		final List<File> result)
+	{
 		final File[] list = directory.listFiles();
 		if (list == null) return;
 		for (final File file : list) {
@@ -277,19 +259,17 @@ public abstract class LegacyHooks {
 	 * Extension point to enhance ImageJ 1.x' error reporting upon
 	 * {@link NoSuchMethodError}.
 	 * 
-	 * @param e
-	 *            the exception to handle
+	 * @param e the exception to handle
 	 * @return true if the error was handled by the legacy hook
 	 */
-	public boolean handleNoSuchMethodError(NoSuchMethodError e) {
+	public boolean handleNoSuchMethodError(final NoSuchMethodError e) {
 		return false; // not handled
 	}
 
 	/**
 	 * Extension point to run after a new PluginClassLoader was initialized.
 	 * 
-	 * @param loader
-	 *            the PluginClassLoader instance
+	 * @param loader the PluginClassLoader instance
 	 */
 	public void newPluginClassLoader(final ClassLoader loader) {
 		// do nothing
