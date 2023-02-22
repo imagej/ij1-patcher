ecca85cee900f683e2c3e4fa64064647b85ad386
Curtis Rueden <ctrueden@wisc.edu>
2023-02-22 17:58:21 -0600

    HeadlessCompletenessTest: tolerate addEnumChoice
    I was not thorough enough in my testing with
    303bf035bf351cbe0df6d9a5b407968371d74824.

--- src/test/java/net/imagej/patcher/HeadlessCompletenessTest.java
+++ src/test/java/net/imagej/patcher/HeadlessCompletenessTest.java
@@ -124,7 +124,8 @@ public class HeadlessCompletenessTest {
 		// d) function correctly while headless
 		for (final String name : new String[] {
 			"actionPerformed(java.awt.event.ActionEvent)", //
-			"addEnumChoice(java.lang.String, java.lang.Enum)", //
+			"addEnumChoice(java.lang.String,java.lang.Enum)", //
+			"addEnumChoice(java.lang.String,java.lang.Enum[],java.lang.Enum)", //
 			"addToSameRow()", //
 			"adjustmentValueChanged(java.awt.event.AdjustmentEvent)", //
 			"getInsets(int,int,int,int)", //
